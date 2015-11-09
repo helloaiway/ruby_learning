76e2dfcf1de7fbef08ba6ffe3de47d9223db1631
helloaiway <active@ML00070141.local>
2015-11-09 09:39:45 +0800

    improve code.

--- p_7.rb
+++ p_7.rb
@@ -1,5 +1,9 @@
 require 'fileutils'
 require 'date'
+
+PermissionError = Class.new(IOError)
+
+
 class Logfile
 	
 	def initialize
@@ -7,48 +11,44 @@ class Logfile
 	end
 
 	# create a file, write a word into it, read id out,
-	def write filename = '0', data = ''
+	def write(data = '', filename = '0')	
 		file_path = get_dirs(time_to_dir)  + '/' + filename
-		begin
-			efile = File.open file_path,'a+'
-			if efile
-				efile.syswrite "#{data}\r"
-				p "write successfully. File path:#{file_path}"
-			else
-				p 'open file failed'
-			end
-		rescue => e
-			p e.inspect
-			p 'can open the file'
+		efile = File.open file_path,'a+'
+		if efile
+			efile.syswrite "#{data}\r"
+			p "write successfully. File path:#{file_path}"
+		else
+			p 'Open file failed.'
 		end
+	rescue PermissionError
+		p "Permission limited. Can not create directory in #{@log_dir}"
+	rescue => e
+		p e.inspect
+		p 'Failed! Unable to open the file.'
 
 	end
 
-	def time_to_dir time = nil
-		begin
-			time_obj = time.nil? ? DateTime.now : DateTime.parse(time)
-			return "#{time_obj.year}/#{time_obj.month}/#{time_obj.day}"
-		rescue => e
-			p e.inspect
-			p 'Can not parse time.'
-			raise 'Parse error'
-		end
+	def time_to_dir(time = nil)
+		time_obj = time.nil? ? DateTime.now : DateTime.parse(time)
+		return time_obj.strftime('%Y/%m/%d') #{}"#{time_obj.year}/#{time_obj.month}/#{time_obj.day}"
+	rescue => e
+		p e.inspect
+		raise ArgumentError
 	end
 
-	def get_dirs dir
-		begin
-			path = FileUtils.mkdir_p(@log_dir + dir)
-			if path
-				path[0]
-			end
-		rescue => e
-			p e.inspect
-		end	
+	def get_dirs(dir)
+		path = FileUtils.mkdir_p(@log_dir + dir)
+		if path
+			path[0]
+		end
+	rescue => e
+		p e.inspect
+		raise PermissionError
 	end
 
 	# create three directories, delete all the files in this directroy,according to the data
-	def read time = '', file
-		file_path = "#{@log_dir}/#{time_to_dir time}/#{file}"
+	def read(file, time = nil)
+		file_path = "#{@log_dir}" + time_to_dir(time) + "/#{file}"
 		if File.readable? file_path
 			efile = File.new(file_path, 'r')
 			if efile
@@ -57,7 +57,17 @@ class Logfile
 		else
 			p "File #{file_path} not exist."
 		end
+	rescue ArgumentError
+		p "The second paramter is not a correct time format."
+	rescue => e
+		p e.message
+	ensure
+		p "Paramters example: file:'test', time:'2015-11-4"	
 
 	end
 
 end
+
+logger = Logfile.new
+#logger.write Time.now.to_s
+#logger.read '0', DateTime.now.strftime('%Y-%m-%d')
